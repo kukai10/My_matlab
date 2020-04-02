dc198a9424ae62393b4d40bea1f247dd7430da25
KukaiN <50426885+kukai10@users.noreply.github.com>
2020-04-02 17:16:34 -0400

    Update BS_Formula.m

--- BS_Formula.m
+++ BS_Formula.m
@@ -1,8 +1,3 @@
-function [xn] = secant_method(xnm1, xn, f, tol, maxn)
-    if nargin < 5 %default parameter values
-        [xnm1, xn, tol, f, maxn] = deal(0, 1, 10^-10, @(x) x^3+x-1, 100);
-    end;
-    i = 1; % initialized counter
-    while (i<maxn+1) && (abs(xnm1-xn) > tol) % loop just assign values iteratively 
-        [xnm1, xn, i] = deal(xn, vpa((xn-(xn-xnm1)*f(xn)/(f(xn)-f(xnm1)))), i+1);
-    end; % exit condition is counter and tolerance vs their respective threshold value
+function [V] = BS_Formula(id, K, t, S, sigma, q, r)
+    d1 = log(S/K)+t*(r-q+(sigma^2)/2)
+    d2 = d1-sigma*sqrt(t)
